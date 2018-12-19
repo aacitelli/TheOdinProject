1a77185db42642a6a114d5c95e8a5d6010d12b79
Anden Acitelli <a.acitelli@gmail.com>
2018-12-19 18:29:17 -0500

    Implemented HTML/Most of CSS for EtchASketch

---
+++ EtchASketch/canvas.js
@@ -0,0 +1,11 @@
+/*
+
+    General Planning: 
+
+    For each box, assign event listeners.
+
+    When the mouse enters, modify the color to something different.
+    That's about all there is to it at a basic level. Can't wait until
+    it ends up being harder than I thought. 
+    
+*/
